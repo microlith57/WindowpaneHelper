3eea7a7fb21455f41edc80976c1b933e7f5d76e7
microlith57 <microlith57@gmail.com>
2022-02-10 12:15:39 +1300

    allow "windowpanehelperonly" tag to dsable backdrop rendering; allow multiple tags per backdrop (#6)

--- WindowpaneHelperModule.cs
+++ WindowpaneHelperModule.cs
@@ -1,40 +1,60 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using Monocle;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 
 namespace Celeste.Mod.WindowpaneHelper {
     public class WindowpaneHelperModule : EverestModule {
         public static WindowpaneHelperModule Instance;
-        private static On.Celeste.Backdrop.hook_Update backdropUpdateHook;
-        private static On.Celeste.Backdrop.hook_Render backdropRenderHook;
+        private static On.Celeste.Backdrop.hook_IsVisible backdropIsVisibleHook;
+        private static On.Celeste.MapData.hook_ParseBackdrop backdropParseHook;
 
         public WindowpaneHelperModule() {
             Instance = this;
         }
 
         public override void Load() {
-            On.Celeste.Backdrop.Update += (backdropUpdateHook = (On.Celeste.Backdrop.orig_Update orig, Backdrop self, Scene scene) => {
-                if (self.Tags.Intersect(Windowpane.GroupLeaderInfo.Keys).Any()) {
-                    bool orig_Visible = self.Visible, orig_ForceVisible = self.ForceVisible;
-                    self.Visible = true;
-                    self.ForceVisible = true;
-                    orig(self, scene);
-                    self.Visible = orig_Visible;
-                    self.ForceVisible = orig_ForceVisible;
-                } else { orig(self, scene); }
-            });
+            IL.Celeste.BackdropRenderer.Render += modBackdropRendererRender;
 
-            On.Celeste.Backdrop.Render += (backdropRenderHook = (On.Celeste.Backdrop.orig_Render orig, Backdrop self, Scene scene) => {
-                if (!ForcefulBackdropRenderer.Rendering || self.Tags.Intersect(Windowpane.GroupLeaderInfo.Keys).Any()) {
-                    orig(self, scene);
+            On.Celeste.Backdrop.IsVisible += (backdropIsVisibleHook = (On.Celeste.Backdrop.orig_IsVisible orig, Backdrop self, Level level) =>
+                self.Tags.Contains("windowpanehelperonly") ||
+                self.Tags.Intersect(Windowpane.GroupLeaderInfo.Keys).Any() ||
+                orig(self, level)
+            );
+
+            On.Celeste.MapData.ParseBackdrop += (backdropParseHook = (On.Celeste.MapData.orig_ParseBackdrop orig, MapData self, BinaryPacker.Element child, BinaryPacker.Element above) => {
+                Backdrop backdrop = orig(self, child, above);
+                if (backdrop.Tags.Count == 1) {
+                    backdrop.Tags = new HashSet<string>(backdrop.Tags.First().Split(','));
                 }
+                return backdrop;
             });
         }
 
         public override void Unload() {
-            On.Celeste.Backdrop.Update -= backdropUpdateHook;
-            On.Celeste.Backdrop.Render -= backdropRenderHook;
+            IL.Celeste.BackdropRenderer.Render -= modBackdropRendererRender;
+            On.Celeste.Backdrop.IsVisible -= backdropIsVisibleHook;
+            On.Celeste.MapData.ParseBackdrop -= backdropParseHook;
+        }
+
+        private void modBackdropRendererRender(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            Logger.Log("WindowpaneHelper", "Patching BackdropRenderer.Render");
+
+            // go just after `if (backdrop.Visible) {`
+            if (!cursor.TryGotoNext(MoveType.After,
+                                    instr => instr.MatchLdloc(2),
+                                    instr => instr.MatchLdfld(typeof(Backdrop), "Visible"),
+                                    instr => instr.MatchBrfalse(out ILLabel _))) { return; }
+            // find the end of the if block
+            cursor.Prev.MatchBrfalse(out ILLabel continue_label);
+
+            // if the backdrop has the tag, jump over the if block
+            cursor.Emit(OpCodes.Ldloc, 2);
+            cursor.EmitDelegate<Func<Backdrop, bool>>(backdrop => backdrop.Tags.Contains("windowpanehelperonly"));
+            cursor.Emit(OpCodes.Brtrue_S, continue_label);
         }
     }
 }
